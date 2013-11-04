2fa27c1dcde1e7a2d263ccd5c786034dcfed3712
mishadoff <mishadoff@gmail.com>
2013-11-04 19:11:27 +0200

    Small fixes

--- src/langolier/train.clj
+++ src/langolier/train.clj
@@ -1,8 +1,10 @@
 (ns langolier.train
-  (:require [langolier.tokenizer :as t]))
+  (:require [langolier.tokenizer :as t])
+  (:require [clojure.java.io :as io]))
 
 (def global-map (atom {:default 1}))
 (def cat-map (atom {}))
+(def DIR "resources/")
 
 (defn train [source language]
   (let [freq (frequencies (t/tokenize source))]
@@ -12,18 +14,30 @@
                    (merge-with + freq (get @cat-map language {}))))
     :ok))
 
-(defn log-prob [token language]
+(defn train-dir [dir]
+  (let [langs (->> dir
+                   (io/file)
+                   (.listFiles)
+                   (map #(.getName %)))]
+    (doseq [lang langs]
+      (let [lang-key (keyword lang)
+            lang-dir (str dir lang)]
+        (doseq [f (remove #(.isDirectory %) 
+                          (file-seq (io/file lang-dir)))]
+          (train (slurp f) lang-key))))))
+  
+(defn- log-prob [token language]
   (Math/log
    (/ (double (inc (get (get @cat-map language {}) token 0)))
       (+ (get @global-map token 0) (reduce + (vals @global-map))))))
 
-(defn score [tokens language]
+(defn- score [tokens language]
   (reduce +' (map #(log-prob % language) tokens))) 
 
-(defn scores [tokens languages]
+(defn- scores [tokens languages]
   (map #(vec [% (score tokens %)]) languages))
 
-(defn best [scores]
+(defn- best [scores]
   (first (apply max-key second scores)))
 
 (defn classify [source]
@@ -31,3 +45,6 @@
       (t/tokenize)
       (scores (keys @cat-map))
       (best)))
+
+(defn languages []
+  (keys @cat-map))
