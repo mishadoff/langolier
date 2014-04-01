f47376125abd93ec73e9cc90747f8678598cc566
unknown <Mykhailo_Kozik@EPUAKYIW0818.kyiv.epam.com>
2014-04-01 11:48:42 +0300

    Gist authorization

--- src/langolier/github.clj
+++ src/langolier/github.clj
@@ -2,12 +2,24 @@
   (:require [clj-http.client :as http])
   (:require [clojure.data.json :as json]))
 
+;; TODO Could be a part of buldozer
+
+;; set this variables or provide bindings
+(def ^:dynamic *username* nil)
+(def ^:dynamic *password* nil)
+
+(defn- auth-map []
+  (if (and *username*
+           *password*) {:basic-auth
+                        [*username* *password*]}
+           {}))
+
 (defn get-raw-links [page]
   "return list of pairs [language raw_url] from gist page"
-  (->> (str "https://api.github.com/gists?page=" page)
-       (http/get)
+  (->> (str "https://api.github.com/gists/public?page=" page)
+       (#(http/get % (auth-map)))
        (:body)
-       (json/read-str)
+       (json/read-str) ;; TODO key-fn
        (map #(get % "files"))
        (mapcat (fn [m]
                  (map (fn [[k v]]
@@ -17,5 +29,6 @@
 
 (defn process []
   (reduce #(apply conj %1 (get-raw-links %2))
-          []
-          (range 1 100)))
+          [] (range 1 100)))
+   
+   
